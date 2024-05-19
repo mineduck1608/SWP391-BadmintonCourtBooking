33920636e918408b1d27f287c4adc6055e1ce3e3
mineduck1608 <155410303+mineduck1608@users.noreply.github.com>
2024-05-19 14:23:40 +0700

    Add files via upload

---
+++ src/Components/googleSignin/config.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnrdte2F_VtOXJjqaq4m7cUgzIvYe3MaU",
+  authDomain: "badmintoncourtbooking-183b2.firebaseapp.com",
+  projectId: "badmintoncourtbooking-183b2",
+  storageBucket: "badmintoncourtbooking-183b2.appspot.com",
+  messagingSenderId: "859078707099",
+  appId: "1:859078707099:web:185eae36409c2810be833e",
+  measurementId: "G-51XHF6B2B5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+export {auth, provider};
