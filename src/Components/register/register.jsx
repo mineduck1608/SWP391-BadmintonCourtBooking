67eccef58255d152f04ef4c06bdff359dc9e8193
mineduck1608 <155410303+mineduck1608@users.noreply.github.com>
2024-05-21 21:15:17 +0700

    Add files via upload

---
+++ src/Components/register/register.jsx
@@ -0,0 +1,26 @@
+import './register.css';
+
+const Register = () => {
+    return (
+        <div className='wrapper'>
+        <form action="">
+          <h1>User Registeration</h1>
+          <div  className="input-register">
+          <div>
+            <input type="text" placeholder="Username" required/>
+            <input type="Password" placeholder="Password" id="password" required/>
+            <input type="Password" placeholder="Confirm password" required/>
+          </div>
+          <div>
+            <input type="text" placeholder="First name" required/>
+            <input type="text" placeholder="Last name" required/>
+            <input type="email" placeholder="Email" required/>
+            <input type="phone" placeholder="Phone" required/>
+          </div>
+          </div>
+         </form>
+    </div>
+    );
+  }
+  
+  export default Register
