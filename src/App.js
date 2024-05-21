07ba5fed29121c5a0823d2065fd99caa93c31e18
mineduck1608 <155410303+mineduck1608@users.noreply.github.com>
2024-05-21 21:16:46 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginForm from './Components/LoginForm/LoginForm';
-
+import Register from './Components/register/register';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div>
-      <LoginForm/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginForm/>}></Route>
+          <Route path='/register' element={<Register/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
