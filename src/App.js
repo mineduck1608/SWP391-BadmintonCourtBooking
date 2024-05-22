33e23f1117803e7a8a6aa6583fb51326e82fb8dd
mineduck1608 <155410303+mineduck1608@users.noreply.github.com>
2024-05-22 10:28:11 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LoginForm from './Components/LoginForm/LoginForm';
-import Navbar from './Components/Navbar/Navbar'
-import Home from './Components/Home/Home'
-import Footer from'./Components/Footer/Footer'
-import Popular from './Components/Popular/Popular'
+import Login from './Components/Login/login';
+import Register from './Components/Register/register';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
+import Footer from'./Components/Footer/Footer';
+import Popular from './Components/Popular/Popular';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div><Navbar/><Home/><Popular/><Footer/></div>
+  },
+  {
+    path: '/login',
+    element: <div><Login/></div>
+  },
+  {
+    path: '/register',
+    element: <div><Register/></div>
+  },
+])
+ 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Home/> 
-      <Popular/>
-      <Footer/>
-      {/* <LoginForm/> */}
+      <RouterProvider router={router}/> 
     </div>
   );
 }
