e9af1a816454f5d5b128eb069f00afbeef9cfa3b
ngocdt18 <ngocdtse183959@fpt.edu.vn>
2024-05-27 13:21:14 +0700

    Add files via upload

---
+++ src/BE/DAO/BasicDAO.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+	internal class BasicDAO
+	{
+		private readonly BadmintonCourtContext context;
+		public BasicDAO()
+		{
+			context = context == null ? new BadmintonCourtContext() : context;
+		}
+
+	}
+}
