c2641151c18ff4cdad55e32484967bfa7e5a2f58
mineduck1608 <155410303+mineduck1608@users.noreply.github.com>
2024-05-27 14:17:46 +0700

    Add files via upload

---
+++ src/Components/Header/header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { MdSportsTennis } from "react-icons/md";
+import './header.css';
+import user from '../../Assets/user.jpg';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <a href="./">
+                <h1 className='logo-header'><MdSportsTennis className="icon" />BMTC</h1>
+            </a>
+            <a href="" className="user-pic">
+                <img src={user} alt="Error" />
+            </a>
+            <div className="text-header">
+                <a href="">Home</a>
+                <a href="">Booking</a>
+                <a href="">Time Balance</a>
+                <a href="">Logout</a>
+            </div>
+
+        </div>
+    );
+}
+
+export default Header
