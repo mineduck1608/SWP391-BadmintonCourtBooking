94c6a6f7a14fb1b551f398f0f6e7162937f4ccf1
mineduck1608 <155410303+mineduck1608@users.noreply.github.com>
2024-05-27 14:10:34 +0700

    Add files via upload

---
+++ src/Components/ViewCourtInfo/viewCourtInfo.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "../Header/header";
+import './viewCourtInfo.css'
+import Footer from "../Footer/Footer";
+
+const ViewCourtInfo = () => {
+    return (
+        <div className="viewcourtinfo"> 
+            <h1>asdsadasd</h1>
+            <h1>asdsadasd</h1>
+            <h1>asdsadasd</h1>
+
+            <h1>asdsadasd</h1>
+            <h1>asdsadasd</h1>
+            <h1>asdsadasd</h1>
+            <h1>asdsadasd</h1>
+
+            <h1>asdsadasd</h1>
+            <h1>asdsadasd</h1>
+            <h1>asdsadasd</h1>
+            <h1>asdsadasd</h1>
+            <h1>asdsadasd</h1>
+            <h1>asdsadasd</h1>
+            <h1>asdsadasd</h1>
+            <h1>asdsadasd</h1>
+            <h1>asdsadasd</h1>
+        </div>
+    );
+}
+
+export default ViewCourtInfo
