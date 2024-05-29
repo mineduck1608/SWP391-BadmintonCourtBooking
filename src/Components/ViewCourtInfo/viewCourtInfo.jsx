53b072a157b6291164ed966dbfc91976e3d222c3
mineduck1608 <ducdm160804@gmail.com>
2024-05-29 20:04:34 +0700

    ds

--- src/Components/ViewCourtInfo/viewCourtInfo.jsx
+++ src/Components/ViewCourtInfo/viewCourtInfo.jsx
@@ -8,7 +8,7 @@ const ViewCourtInfo = () => {
     return (
         <div className="viewcourtinfo">
             <div>
-                <Header />
+                <Header/>
             </div>
             <div className="viewcourtinfo-body">
                 <div className="viewcourtinfo-body-pic">
@@ -27,6 +27,41 @@ const ViewCourtInfo = () => {
                     <p>Hi chung minh la</p>
                 </div>
             </div>
+            <div className="viewcourtinfo-body">
+                <div className="viewcourtinfo-body-pic">
+                    <img className="viewcourtinfo-img" src={image2} alt="" />
+                    <div className="viewcourtinfo-info">
+
+                        <h2>Name: </h2>
+                        <p>Address: Name</p>
+                        <p>Time: AAAAA</p>
+                        <p>Branch: BBBB</p>
+                        <p>Status: FREE</p>
+                    </div>
+                </div>
+                <div className="viewcourtinfo-body-des">
+                    <h1>Description:</h1>
+                    <p>Hi chung minh la</p>
+                </div>
+            </div>
+            <div className="viewcourtinfo-body">
+                <div className="viewcourtinfo-body-pic">
+                    <img className="viewcourtinfo-img" src={image2} alt="" />
+                    <div className="viewcourtinfo-info">
+
+                        <h2>Name: </h2>
+                        <p>Address: Name</p>
+                        <p>Time: AAAAA</p>
+                        <p>Branch: BBBB</p>
+                        <p>Status: FREE</p>
+                    </div>
+                </div>
+                <div className="viewcourtinfo-body-des">
+                    <h1>Description:</h1>
+                    <p>Hi chung minh la</p>
+                </div>
+            </div>
+            
             <div className="viewcourtinfo-feedback">
 
             </div>
