7503d1a0c444c8a260a8fd923adf3ab8dcd65d2f
mineduck1608 <ducdm160804@gmail.com>
2024-06-30 01:01:21 +0700

    1

--- BadmintonCourt_Booking/BadmintonCourtAPI/obj/Debug/net8.0/BadmintonCourtAPI.AssemblyInfo.cs
+++ BadmintonCourt_Booking/BadmintonCourtAPI/obj/Debug/net8.0/BadmintonCourtAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadmintonCourtAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0af3440ba8239253ccbcf8cc75eb24c540bcd4ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32e2de7c5d1f9ae33f4f7dfe916522db3e272357")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadmintonCourtAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadmintonCourtAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
