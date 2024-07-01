7d8e50f97c5d10d3b935726de1372e6cd33d9f4b
mineduck1608 <ducdm160804@gmail.com>
2024-07-01 22:59:22 +0700

    1

--- BadmintonCourt_Booking/BadmintonCourtAPI/Program.cs
+++ BadmintonCourt_Booking/BadmintonCourtAPI/Program.cs
@@ -1,92 +1,3 @@
-
-//using BadmintonCourtServices;
-//using BadmintonCourtServices.IService;
-//using Microsoft.AspNetCore.Authentication.Cookies;
-//using Microsoft.AspNetCore.Authentication.Google;
-//using Microsoft.AspNetCore.Authentication.JwtBearer;
-//using Microsoft.IdentityModel.Tokens;
-//using System.Configuration;
-//using System.Text;
-//namespace BadmintonCourtAPI
-//{
-//	public class Program
-//	{
-//		public static void Main(string[] args)
-//		{
-
-//			var builder = WebApplication.CreateBuilder(args);
-
-
-//			// Add services to the container.
-
-//			builder.Services.AddControllers();
-//			builder.Services.AddCors(options =>
-//			{
-//				options.AddPolicy("AllowAll", builder =>
-//				{
-//					builder.AllowAnyOrigin()
-//						   .AllowAnyMethod()
-//						   .AllowAnyHeader();
-//				});
-//			});
-//			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//			builder.Services.AddEndpointsApiExplorer();
-//			builder.Services.AddSwaggerGen();
-
-
-//			builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-//			{
-//				options.TokenValidationParameters = new TokenValidationParameters
-//				{
-//					ValidateIssuer = true,
-//					ValidateAudience = true,
-//					ValidateLifetime = true,
-//					ValidateIssuerSigningKey = true,
-//					ValidIssuer = builder.Configuration["Jwt:Issuer"],
-//					ValidAudience = builder.Configuration["Jwt:Audience"],
-//					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-//				};
-//			});
-//			builder.Services.AddRazorPages();
-//			builder.Services.AddMvc(); 
-
-//			builder.Services.AddSingleton<IVnPayService, VnPayService>();
-
-//			var app = builder.Build();
-
-//			if (app.Environment.IsDevelopment())
-//			{
-//				app.UseSwagger();
-//				app.UseSwaggerUI();
-//				app.UseDeveloperExceptionPage();
-//			}
-
-//			app.UseHttpsRedirection();
-//			app.UseStaticFiles();
-
-//			app.UseAuthentication();
-
-//			app.UseRouting();
-
-//			// Place app.UseAuthorization() before app.UseEndpoints(...)
-//			app.UseAuthorization();
-//			app.UseCors("AllowAll");
-
-
-//			app.UseEndpoints(endpoints =>
-//			{
-//				endpoints.MapControllers();
-//				endpoints.MapRazorPages();
-//			});
-
-//			app.Run();
-
-//		}
-//	}
-//}
-
-
-
 using BadmintonCourtBusinessDAOs;
 using BadmintonCourtServices;
 using BadmintonCourtServices.IService;
@@ -97,97 +8,107 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
-using System.Configuration;
 using System.Text;
 
 namespace BadmintonCourtAPI
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-			builder.Services.AddControllers();
-			var isTesting = builder.Configuration.GetValue<bool>("UseInMemoryDatabase");
-			if (isTesting)
-			{
-				builder.Services.AddDbContext<BadmintonCourtContext>(options =>
-					options.UseInMemoryDatabase("InMemoryDb"));
-			}
+            // Add services to the container.
+            builder.Services.AddControllers();
 
-			builder.Services.AddCors(options =>
-			{
-				options.AddPolicy("AllowAll", builder =>
-				{
-					builder.AllowAnyOrigin()
-						   .AllowAnyMethod()
-						   .AllowAnyHeader();
-				});
-			});
+            var isTesting = builder.Configuration.GetValue<bool>("UseInMemoryDatabase");
+            if (isTesting)
+            {
+                builder.Services.AddDbContext<BadmintonCourtContext>(options =>
+                    options.UseInMemoryDatabase("InMemoryDb"));
+            }
+            else
+            {
+                // Add your production database context here
+                // builder.Services.AddDbContext<BadmintonCourtContext>(options =>
+                //     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            }
 
-			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen(options =>
-			{
-				options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-				{
-					Description = "Standard Authorization header using Bearer scheme",
-					In = ParameterLocation.Header,
-					Name = "Authorization",
-					Type = SecuritySchemeType.ApiKey
-				});
+            // Configure CORS policy
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policyBuilder =>
+                {
+                    policyBuilder.AllowAnyOrigin()
+                                 .AllowAnyMethod()
+                                 .AllowAnyHeader();
+                });
+            });
 
-				options.OperationFilter<SecurityRequirementsOperationFilter>();
-			});
+            // Configure Swagger/OpenAPI
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization header using Bearer scheme",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
 
-			builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-			{
-				options.TokenValidationParameters = new TokenValidationParameters
-				{
-					ValidateIssuer = true,
-					ValidateAudience = true,
-					ValidateLifetime = true,
-					ValidateIssuerSigningKey = true,
-					ValidIssuer = builder.Configuration["Jwt:Issuer"],
-					ValidAudience = builder.Configuration["Jwt:Audience"],
-					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-				};
-			});
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
 
-			builder.Services.AddRazorPages();
-			builder.Services.AddMvc();
+            // Configure authentication
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
 
-			builder.Services.AddSingleton<IVnPayService, VnPayService>();
+            builder.Services.AddRazorPages();
+            builder.Services.AddMvc();
 
-			var app = builder.Build();
+            builder.Services.AddSingleton<IVnPayService, VnPayService>();
 
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-				app.UseDeveloperExceptionPage();
-			}
+            var app = builder.Build();
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
+            }
 
-			app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-			app.UseRouting();
+            app.UseRouting();
 
-			// Place app.UseAuthorization() before app.UseEndpoints(...)
-			app.UseAuthorization();
-			app.UseCors("AllowAll");
+            // Authentication must come before Authorization
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllers();
-				endpoints.MapRazorPages();
-			});
+            // CORS policy must be applied before the endpoints are mapped
+            app.UseCors("AllowAll");
 
-			app.Run();
-		}
-	}
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
+            });
+
+            app.Run();
+        }
+    }
 }
