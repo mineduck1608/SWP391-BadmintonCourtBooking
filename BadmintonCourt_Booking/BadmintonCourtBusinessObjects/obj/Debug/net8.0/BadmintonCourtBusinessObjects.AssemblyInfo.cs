532c82ed2e5c56800221ef29c3d3c275f425c6ce
mineduck1608 <ducdm160804@gmail.com>
2024-07-03 15:24:27 +0700

    1

--- BadmintonCourt_Booking/BadmintonCourtBusinessObjects/obj/Debug/net8.0/BadmintonCourtBusinessObjects.AssemblyInfo.cs
+++ BadmintonCourt_Booking/BadmintonCourtBusinessObjects/obj/Debug/net8.0/BadmintonCourtBusinessObjects.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadmintonCourtBusinessObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b50e64b0a440a8463b3e67b671354740dad91602")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a14e65a42726cebf54ca0839edebddb19490d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadmintonCourtBusinessObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadmintonCourtBusinessObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BadmintonCourt_Booking/BadmintonCourtRepositories/obj/Debug/net8.0/BadmintonCourtServices.AssemblyInfo.cs
+++ BadmintonCourt_Booking/BadmintonCourtRepositories/obj/Debug/net8.0/BadmintonCourtServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadmintonCourtServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b50e64b0a440a8463b3e67b671354740dad91602")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a14e65a42726cebf54ca0839edebddb19490d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadmintonCourtServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadmintonCourtServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
