dba29e03c77c11582b4425b360d49c2e92b11e8c
Phuchtq <taikhoanhoconl123@gmail.com>
2024-07-10 20:43:35 +0700

    Little fix of user updating

--- BadmintonCourt_Booking/BadmintonCourtBusinessObjects/obj/Debug/net8.0/BadmintonCourtBusinessObjects.AssemblyInfo.cs
+++ BadmintonCourt_Booking/BadmintonCourtBusinessObjects/obj/Debug/net8.0/BadmintonCourtBusinessObjects.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadmintonCourtBusinessObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0254f573715b2b2719e03298ce99273d5289e88a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88077267ef0dcced7918283ca1dbbf2f0cc363f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadmintonCourtBusinessObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadmintonCourtBusinessObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BadmintonCourt_Booking/BadmintonCourtRepositories/obj/Debug/net8.0/BadmintonCourtServices.AssemblyInfo.cs
+++ BadmintonCourt_Booking/BadmintonCourtRepositories/obj/Debug/net8.0/BadmintonCourtServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadmintonCourtServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0254f573715b2b2719e03298ce99273d5289e88a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88077267ef0dcced7918283ca1dbbf2f0cc363f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadmintonCourtServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadmintonCourtServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
