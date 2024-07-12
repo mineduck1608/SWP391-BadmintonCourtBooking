b3e9d0d8e34244dd88292d12a87a09733f4f1ac9
mineduck1608 <ducdm160804@gmail.com>
2024-07-12 18:23:07 +0700

    1

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -73,7 +73,7 @@ const Header = () => {
     };
 
     const toggleDropdown = () => {
-        setDropdownOpen(!dropdownOpen);
+        setDropdownOpen(prevState => !prevState);
     };
 
     return (
