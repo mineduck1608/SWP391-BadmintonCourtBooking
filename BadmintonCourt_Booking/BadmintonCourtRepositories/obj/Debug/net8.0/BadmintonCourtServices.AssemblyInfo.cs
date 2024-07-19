fe238083f8bd1d31d8a36505b95d0030313f754c
Phuchtq <taikhoanhoconl123@gmail.com>
2024-07-20 01:12:43 +0700

    Little fix of mail service and caculate distance

--- BadmintonCourt_Booking/BadmintonCourtRepositories/obj/Debug/net8.0/BadmintonCourtServices.AssemblyInfo.cs
+++ BadmintonCourt_Booking/BadmintonCourtRepositories/obj/Debug/net8.0/BadmintonCourtServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadmintonCourtServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29e6b8d0322b3adaec95ff5c3291b9b7ea742d00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d7b892dffcd8b8c7e0fa0b657f6a6d9f5c113cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadmintonCourtServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadmintonCourtServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
