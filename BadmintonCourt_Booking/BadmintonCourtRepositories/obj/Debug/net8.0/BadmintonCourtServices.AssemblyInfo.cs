29e6b8d0322b3adaec95ff5c3291b9b7ea742d00
Phuchtq <taikhoanhoconl123@gmail.com>
2024-07-19 14:50:09 +0700

    Update changelog

--- BadmintonCourt_Booking/BadmintonCourtRepositories/obj/Debug/net8.0/BadmintonCourtServices.AssemblyInfo.cs
+++ BadmintonCourt_Booking/BadmintonCourtRepositories/obj/Debug/net8.0/BadmintonCourtServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadmintonCourtServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3e525dc4011c1fa2e8420cf892b06c4087051c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+240bdadb78e4a59e043273df41f9735742c59179")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadmintonCourtServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadmintonCourtServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
