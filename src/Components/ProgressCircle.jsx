ff79a9b73d2fca5f1d8c05a1df3a47e0b755a5ec
mineduck1608 <ducdm160804@gmail.com>
2024-07-22 22:35:48 +0700

    1

--- src/Components/ProgressCircle.jsx
+++ src/Components/ProgressCircle.jsx
@@ -1,15 +1,45 @@
+import React, { useState, useEffect } from "react";
 import { Box, useTheme } from "@mui/material";
 import { tokens } from "../theme";
+import { fetchWithAuth } from "./fetchWithAuth/fetchWithAuth";
 
-const ProgressCircle = ({ progress = "0.75", size = "40" }) => {
+const ProgressCircle = ({ size = 40 }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const [progress, setProgress] = useState(0);
+  const token = sessionStorage.getItem('token');
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetchWithAuth("https://localhost:7233/Slot/CancelStatistic?id=0&year=2024", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}` // Thay YOUR_AUTH_TOKEN bằng mã thông báo thực tế của bạn
+          }
+        });
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        // Giả sử giá trị progress bạn nhận được từ API là `data.progress`
+        setProgress(data.progress); // Cập nhật giá trị progress
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const angle = progress * 360;
+
   return (
     <Box
       sx={{
         background: `radial-gradient(${colors.primary[400]} 55%, transparent 56%),
-            conic-gradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}deg 360deg),
+            conic-gradient(${colors.blueAccent[500]} 0deg ${angle}deg, transparent ${angle}deg 360deg),
             ${colors.greenAccent[500]}`,
         borderRadius: "50%",
         width: `${size}px`,
